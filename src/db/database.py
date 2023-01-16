03ed0154d9bbd778f635c27e2231a39ccb208de0
Sergey <nsergeym@gmail.com>
2023-01-16 23:56:56 +0300

    CRUD(Menu, Submenu, Dishes)

--- src/db/database.py
+++ src/db/database.py
@@ -1,6 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from core import config
+from sqlalchemy.orm import sessionmaker
 
 engine = create_engine(config.DATABASE_URL, echo=True)
 db = declarative_base()
+sessionLocal = sessionmaker(autocommit=False,
+                            autoflush=False,
+                            bind=engine)
+
+
+def get_db():
+    db_local = sessionLocal()
+    try:
+        yield db_local
+    finally:
+        db_local.close()
+   
---
+++ src/services/menu.py
@@ -0,0 +1,42 @@
+from sqlalchemy.orm import Session
+
+from api.schemas.menu_schema import MenuCreate
+from models.menu import MenuModel
+
+
+def get_list_menu(db: Session):
+    data = db.query(MenuModel).all()
+    return data
+
+
+def get_menu_by_title(db: Session, title: str):
+    data = db.query(MenuModel).filter(MenuModel.title == title).first()
+    return data
+
+
+def get_menu_by_id(db: Session, id_: int):
+    data = db.query(MenuModel).filter(MenuModel.id == id_).first()
+    return data
+
+
+def create_menu(db: Session, menu: MenuCreate):
+    data = MenuModel(**menu.dict())
+    db.add(data)
+    db.commit()
+    db.refresh(data)
+    return data
+
+
+def update_menu(db: Session, id_: int):
+    db.commit()
+    return get_menu_by_id(db=db, id_=id_)
+
+
+def delete_menu(db: Session, id_: int):
+    data = get_menu_by_id(db, id_)
+    if data is None:
+        return "Menu does not exist"
+    else:
+        db.delete(data)
+        db.commit()
+        return f"Menu - {id_} delete, ok!"
