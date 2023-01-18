ae401dccf4ff2d2ad5817f0a35e7f6c838bacd15
Sergey <nsergeym@gmail.com>
2023-01-18 10:37:07 +0300

    Update README.md

--- menuapp/database.py
+++ menuapp/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://sergey:2709@localhost:5432/my_menu"
+SQLALCHEMY_DATABASE_URL = "postgresql://user:password@host:port/dbname"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
